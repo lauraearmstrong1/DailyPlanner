77ca33b001b2384797cb2c1f11feea5c4596106f
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-02-21 19:34:17 -0500

    added loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,32 @@ $( document ).ready(function(){
     console.log(date);   
     $("#currentDay").append(date);
 
-    // var nine = $("div").addClass("#9AM");
-    // var ten = $("div").addClass("#10AM");
-  
-    // var arr = ["nine", "ten"];
+    var nine = document.getElementsByClassName("nine");
+    var ten = document.getElementsByClassName("ten");
+    var eleven = document.getElementsByClassName("eleven");
+    var twelve = document.getElementsByClassName("twelve");
+    var one = document.getElementsByClassName("one");
+    var two = document.getElementsByClassName("two");
+    var three = document.getElementsByClassName("three");
+    var four = document.getElementsByClassName("four");
+    var five = document.getElementsByClassName("five");
 
-    // for (var i= 0; i < arr.length; i++) {
-
-    //     if ([i] === moment().format)
-        
-    //}
-
-
-    $("#9AM").addClass("#present");
-
-
-
+    var timeSlot = ["nine", "ten", "eleven", "twelve", "one", "two", "three", "four", "five"]
+    var arr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+    var now  = moment().format("HH");
+    var nineAM = "09"
+    
+    console.log(now);
+    
+    for (var i= 0; i < arr.length; i++) {
+        console.log(arr[i])
+        if (arr[i] < now) {
+            for (var j=0; j < timeSlot.length; j++) {
+                $(timeSlot[j]).css({"background-color": "#d3d3d3"})
+            }
+            
+        }
+    }
    
 
 })
